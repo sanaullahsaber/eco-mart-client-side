16bfb74bc973279fd7324fed4b0d5b30417eeee2
sanaullahsabere <you@example.com>
2024-06-02 23:53:09 +0600

    project setup and all page done

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>This is Error page</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import Shop from "../pages/Shop/Shop";
+import Product from "../pages/Product/Product";
+import Blog from "../pages/Blog/Blog";
+import Contact from "../pages/Contact/Contact";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/shop",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "/product",
+        element: <Product></Product>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+  
+]);
